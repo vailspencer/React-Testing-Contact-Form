20c2bc6148842a195fc629f129e2e9c1004e461d
Spencer_Vail <vail.spencer@gmail.com>
2020-05-07 16:07:05 -0700

    initial commit

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,5 +2,23 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import React from 'react';
+import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import 'mutationobserver-shim';
+import App from './App';
+import ContactForm from './components/ContactForm';
+import {render} from '@testing-library/react';
+
+test('App render First Name, Last name, Email, and Message', () => {
+    const render = rtl.render (<ContactForm />);
+    const FirstName = render.queryByText('First Name*')
+    const LastName = render.queryByText('Last Name*')
+    const Email = render.queryByText('Email*')
+    const Message = render.queryByText('Message');
+
+    expect(FirstName).toBeInTheDocument();
+    expect(LastName).toBeInTheDocument();
+    expect(Email).toBeInTheDocument();
+    expect(Message).toBeInTheDocument();
+});
